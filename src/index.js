ac29df1610df2aaeca5ddca37f2a2d24396a7c3b
Lelik Jan <21alex7777@gmail.com>
2023-09-07 17:16:01 +0300

    feat: implement sticky for favorites

--- src/index.js
+++ src/index.js
@@ -9,6 +9,13 @@ window.addEventListener("load", function () {
   addSliderBtnsHandler();
   addClickArrowLeftHandler();
   addClickArrowRightHandler();
+
+  //sticky favorites
+  if(window.innerWidth<769){
+    window.onscroll = function () {
+      implementSticky();
+    };
+  }
 });
 
 window.addEventListener("resize", function () {
@@ -19,8 +26,29 @@ window.addEventListener("resize", function () {
   _sliderContainer.classList.remove(...arr);
   _sliderContainer.classList.add("one");
   _sliderBtns.forEach((el) => {
-    el.querySelector(".button_round").classList.remove('active');
+    el.querySelector(".button_round").classList.remove("active");
   });
-  _sliderBtns[0].querySelector(".button_round").classList.add('active');
+  _sliderBtns[0].querySelector(".button_round").classList.add("active");
 
+  //sticky for favorites
+  if(window.innerWidth<769){
+    window.onscroll = function () {
+      implementSticky();
+    };
+  }
 });
+
+
+
+
+
+function implementSticky() {
+
+  if (window.pageYOffset >1700&&window.pageYOffset<4000) {
+    UTILS.getElementFromDom(".favorites__inputs").classList.add("sticky");
+    console.log("add sticky");
+  }else {
+    UTILS.getElementFromDom('.favorites__inputs').classList.remove("sticky");
+    console.log(window.pageYOffset);
+  }
+}
