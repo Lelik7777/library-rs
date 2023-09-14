86152697f69d9a36a2298f36056183f701548e3a
Lelik Jan <21alex7777@gmail.com>
2023-09-14 18:50:29 +0300

    fix: fix bug in login window

--- src/js/login.js
+++ src/js/login.js
@@ -1,26 +1,31 @@
 import { CONSTANTS } from "./config";
 import { UTILS } from "./utils";
 
-
 const _email = UTILS.getElementFromDom("#email_login");
 const _password = UTILS.getElementFromDom("#password_login");
 const _btnSignUp = UTILS.getElementFromDom(".login__form .register__button");
 
 const addClickBtnSignUpLoginHandler = () => {
   _btnSignUp.addEventListener(CONSTANTS.CLICK, function (e) {
-  
-if(_email.value&&_password.value){
-const form=JSON.parse(window.localStorage.getItem('form'));
-if((_email.value===form.email||_email.value===form.cardNum)&&_password.value===form.password){
-  form.login=true;
-  window.localStorage.setItem('form',JSON.stringify(form));
-}else{
-  alert('wrong entered data')
-}
-}
+    if (_email.value && _password.value&&_password.value.length>=8) {
+      
+      const form = JSON?.parse(window.localStorage.getItem("form"));
 
+     if (form) {
+      if (form) {
+      }
+      if ((_email.value === form.email || _email.value === form.cardNum) && _password.value === form.password) {
+        form.login = true;
+        window.localStorage.setItem("form", JSON.stringify(form));
+      } else {
+        alert("wrong entered data");
+      }
+     }
+     else{
+      alert('you have to sign up')
+     }
+    }
   });
 };
 
-
-export { addClickBtnSignUpLoginHandler};
+export { addClickBtnSignUpLoginHandler };
